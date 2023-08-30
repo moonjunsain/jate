16ea3cccac5e8d1e9b10dd692df7de534b15f9bd
moonjunsain <moonjunsain@gmail.com>
2023-08-29 19:41:06 -0700

    added value

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, text: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result);
 }
@@ -32,7 +32,7 @@ export const getDb = async () => {
   const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result?.text;
+  return result?.value;
 }
 
 initdb();
